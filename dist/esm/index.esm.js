961b887fb47ccca0c05b221a1cb9d7ae03f5cb10
Geraldo Ramos <gramos@symmetrimarketing.com>
2024-08-21 11:54:34 -0400

    Published branch "main"

---
+++ dist/esm/index.esm.js
@@ -0,0 +1,2 @@
+export{G as default}from"./index-CBf0lMPI.esm.js";import"react";import"react-dom";import"path";
+//# sourceMappingURL=index.esm.js.map
---
+++ dist/server/index.js
@@ -0,0 +1,2 @@
+export{g as getGravityForm}from"./index-DI3GzoQT.js";export{gravityFormQuery,submitMutationQuery}from"./query.js";import"node:http";import"node:https";import"node:zlib";import"node:stream";import"node:buffer";import"node:util";import"node:url";import"node:net";import"node:fs";import"node:path";
+//# sourceMappingURL=index.js.map
